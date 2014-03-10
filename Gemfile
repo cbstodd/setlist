5f118d5037fd6abc55f92c7f938a792bfab45d05
Colin Stodd <cbstodd@gmail.com>
2014-03-10 18:48:56 -0400

    Finish laout and routes and tests

--- Gemfile
+++ Gemfile
@@ -6,13 +6,7 @@ gem 'bcrypt-ruby', '3.0.1'
 gem 'faker', '1.0.1'
 gem 'will_paginate', '3.0.3'
 gem 'bootstrap-will_paginate', '0.0.6'
-gem 'jquery-rails', '2.2.1'
-gem 'therubyracer'
-gem 'less-rails-bootstrap'
-gem 'designmodo-flatuipro-rails'
-gem 'therubyrhino'
-
-
+gem 'jquery-rails', '2.0.2'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
@@ -26,7 +20,7 @@ end
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails',   '3.2.5'
+  gem 'sass-rails', '3.2.5'
   gem 'coffee-rails', '3.2.2'
   gem 'uglifier', '1.2.3'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,52 +2,44 @@ require 'spec_helper'
 
 describe "views/static_pages" do
 
-  describe "Home page" do 
-    it "Should have the content 'Setlist'" do 
-      visit '/static_pages/home'
-      page.should have_selector("h1", text: "Setlist")
+  subject { page }
+
+    describe "Home page" do 
+      before { visit root_path }
+        it { should have_selector("h1",        text: "Setlist") } 
+        it { should have_selector("title",     text: "") }
+        it { should_not have_selector("title", text: "| Home")}
     end
 
-    it "should have the title 'Setlist | Home'" do 
-      visit '/static_pages/home'
-      page.should have_selector("title", text: "Setlist | Home")
-    end
-  end
+    describe "Help page" do
+      before { visit help_path }
+        it { should have_selector("h1",    text: "Help") } 
+        it { should have_selector("title", text: "Setlist | Help") }
+      end
 
-  describe "Help page" do
-    it "Should have the content 'Help'" do 
-      visit '/static_pages/help'
-      page.should have_selector("h1", text: "Help")
-    end
 
-    it "should have the title 'Setlist | Help'" do 
-      visit '/static_pages/help'
-      page.should have_selector("title", text: "Setlist | Help")
-    end
-  end
-
-  describe "About page" do
-    it "Should have the content 'About'" do 
-      visit '/static_pages/about'
-      page.should have_selector("h1", text: "About")
+    describe "About page" do
+      before { visit about_path }
+        it { should have_selector("h1",    text: "About") }
+        it { should have_selector("title", text: "Setlist | About") }
     end
 
-    it "should have the title 'Setlist | About'" do 
-      visit '/static_pages/about'
-      page.should have_selector("title", text: "Setlist | About")
-    end
-  end
-
-  describe "Contact page" do
-    it "Should have the content 'Contact'" do 
-      visit '/static_pages/contact'
-      page.should have_selector("h1", text: "Contact")
+    describe "Contact page" do
+      before { visit contact_path }
+      it { should have_selector("h1",    text: "Contact") }
+      it { should have_selector("title", text: "Setlist | Contact") }
     end
 
-    it "should have the title 'Setlist | Contact'" do 
-      visit '/static_pages/contact'
-      page.should have_selector("title", text: "Setlist | Contact")
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      page.should have_selector "title", text: full_title("About us")
+      click_link "Help"
+      page.should have_selector "title", text: full_title("Help")
+      click_link "Contact"
+      page.should have_selector "title", text: full_title("Contact")
+      click_link "Home"
+      click_link "Sign up now"
+      page.should have_selector "title", text: full_title("Sign up")
     end
-  end
-
 end
