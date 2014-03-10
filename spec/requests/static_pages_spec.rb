16ace1255da4a6405dc51f6f5682e539567422df
Colin Stodd <cbstodd@gmail.com>
2014-03-10 19:19:52 -0400

    Cleaned up merged files

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "views/static_pages" do
 
+  subject { page }
+
     describe "Home page" do 
       before { visit root_path }
         it { should have_selector("h1",        text: "Setlist") } 
